8d06b59c292bd41190e11dcec8c11aac70cf9b86
KerMin4 <g01055764230@gmail.com>
2024-09-12 17:47:58 +0900

    인텔리제이테스트

---
+++ DSA_Project/src/main/java/com/dsa/team1/controller/LoginController.java
@@ -0,0 +1,16 @@
+package com.dsa.team1.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@Controller
+public class LoginController{
+	
+	@GetMapping("login")
+	public String login() {
+		return "login";
+	}
+}
