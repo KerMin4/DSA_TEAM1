9c3b51ec7903ef22f441b7eb880b19dddcf6917a
pinknekokitty <pinknekokitty@gmail.com>
2024-09-17 22:07:49 +0900

    시큐리티 기본 보안 인증 페이지 안 뜨게 하려고 잠깐 해둠

--- DSA_Project/src/main/java/com/dsa/team1/DsaProjectApplication.java
+++ DSA_Project/src/main/java/com/dsa/team1/DsaProjectApplication.java
@@ -2,8 +2,11 @@ package com.dsa.team1;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
-@SpringBootApplication
+//@SpringBootApplication 원래 코드
+@SpringBootApplication(exclude = SecurityAutoConfiguration.class)  
 public class DsaProjectApplication {
 
 	public static void main(String[] args) {
@@ -11,3 +14,10 @@ public class DsaProjectApplication {
 	}
 
 }
+
+
+
+/*
+ *  @SpringBootApplication(exclude = SecurityAutoConfiguration.class)
+ *  위에 코드 Spring Security 시큐리티 기본 보안 인증 페이지 안 뜨게 하려고 잠깐 해둠
+ */
--- DSA_Project/src/main/java/com/dsa/team1/controller/LoginController.java
+++ DSA_Project/src/main/java/com/dsa/team1/controller/LoginController.java
@@ -11,6 +11,6 @@ public class LoginController{
 	
 	@GetMapping("login")
 	public String login() {
-		return "login";
+		return "loginForm";
 	}
 }
