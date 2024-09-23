7fb17e225f6c7ada948d057c710fbc8cd9dd0d7c
KerMin4 <g01055764230@gmail.com>
2024-09-23 15:04:29 +0900

    add Security settings, joinForm

---
+++ DSA_Project/src/main/java/com/dsa/team1/security/AuthenticatedUser.java
@@ -0,0 +1,64 @@
+package com.dsa.team1.security;
+
+import java.util.Collection;
+import java.util.Collections;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Service;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Builder
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class AuthenticatedUser implements UserDetails {
+	
+	private static final long serialVersionUID = -2757521351355190L;
+	
+	String id;
+	String password;
+	String name;
+	String roleName;
+	boolean enabled;
+	
+	@Override
+	public String getPassword() {
+		return this.password;
+	}
+
+	@Override
+	public Collection<? extends GrantedAuthority> getAuthorities() {
+		return Collections.singletonList(new SimpleGrantedAuthority(roleName));
+	}
+
+	@Override
+	public String getUsername() {
+		return this.id;
+	}
+	
+	@Override
+	public boolean isAccountNonExpired() {
+		return true;
+	}
+	
+	@Override
+	public boolean isAccountNonLocked() {
+		return true;
+	}
+	
+	@Override
+	public boolean isCredentialsNonExpired() {
+		return true;
+	}
+	
+	@Override
+	public boolean isEnabled() {
+		return this.enabled;
+	}
+}
