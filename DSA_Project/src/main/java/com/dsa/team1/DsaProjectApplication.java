8d6603c5a3e1ec41c3542945baee63e216691f28
pinknekokitty <pinknekokitty@gmail.com>
2024-09-24 21:27:30 +0900

    sadas

--- DSA_Project/src/main/java/com/dsa/team1/DsaProjectApplication.java
+++ DSA_Project/src/main/java/com/dsa/team1/DsaProjectApplication.java
@@ -2,11 +2,8 @@ package com.dsa.team1;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
-import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
-//@SpringBootApplication 원래 코드
-@SpringBootApplication(exclude = SecurityAutoConfiguration.class)  
+@SpringBootApplication
 public class DsaProjectApplication {
 
 	public static void main(String[] args) {
@@ -14,10 +11,3 @@ public class DsaProjectApplication {
 	}
 
 }
-
-
-
-/*
- *  @SpringBootApplication(exclude = SecurityAutoConfiguration.class)
- *  위에 코드 Spring Security 시큐리티 기본 보안 인증 페이지 안 뜨게 하려고 잠깐 해둠
- */
