f803eeb9b505bac8eef2b8281f7ea2eed38f473a
yj-inven <dpwlsl041873@gmail.com>
2024-10-01 14:05:03 +0900

    update sql and delete all entities and add new entities

--- DSA_Project/src/main/java/com/dsa/team1/entity/NotificationEntity.java
+++ DSA_Project/src/main/java/com/dsa/team1/entity/NotificationEntity.java
@@ -1,6 +1,11 @@
 package com.dsa.team1.entity;
 
-import java.sql.Timestamp;
+import java.time.LocalDateTime;
+
+import org.hibernate.usertype.UserType;
+import org.springframework.data.annotation.CreatedDate;
+
+import com.dsa.team1.entity.enums.JoinMethod;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
@@ -10,25 +15,40 @@ import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
+/**
+ * Page		Top right of all logged-in pages
+ * Function	Notify group membership, etc
+ * @version	CreateTables_6
+ */
 @Entity
+@Builder
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 @Table(name = "Notification")
 public class NotificationEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long notificationId;
+    @Column(name = "notification_id")
+    private Integer notificationId;
 
     @ManyToOne
     @JoinColumn(name = "user_id")
     private UserEntity user;
 
+    @Column(columnDefinition = "text")
     private String message;
-
-    @Column(name = "read_status")
+    
+    @Column(name = "read_status", columnDefinition = "Boolean Default false")
     private Boolean readStatus = false;
+    
+    @CreatedDate
+	@Column(name = "created_at", columnDefinition = "timestamp default current_timestamp")
+    private LocalDateTime createdAt;
 
-    @Column(name = "created_at", updatable = false)
-    private Timestamp createdAt;
-
-    // Getters and Setters
 }
